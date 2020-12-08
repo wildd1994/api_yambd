5548cc9f24b97e2d8a9908f60fd2bf566bce31e7
Георгий <dfffgfgf@yandex.ru>
2020-12-08 20:21:17 +0300

    create models: Categories, Genres and Titles

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=150)
+    year = models.DecimalField(max_digits=4)
+    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, related_name='categories')
+    genres = models.ManyToManyField(Genres)
