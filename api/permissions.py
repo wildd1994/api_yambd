6ad8e81ebb8b4dd7fb8fb6ea195f225aec2c865f
Георгий <dfffgfgf@yandex.ru>
2020-12-09 15:04:35 +0300

    fix permissions.py category test passed

--- api/permissions.py
+++ api/permissions.py
@@ -26,6 +26,6 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
 
         if request.user.is_authenticated:
-            return request.user.is_admin
+            return request.user.role == 'admin'
 
         return False
