04232d6ff9fcdbdae28a8c262b90beecdb6982c0
Vadim <wadim-egoroff@yandex.ru>
2020-12-16 00:10:46 +0300

    Changes after the user review mini fix

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser  # , BaseUserManager
+from django.contrib.auth.models import AbstractUser  # , BaseUserManager
 
 
 class User(AbstractUser):
